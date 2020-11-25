8b77ca05d9bbeb663b7867f75ef02d7226f75844
maduoma <18477889+maduoma@users.noreply.github.com>
2020-11-25 23:14:09 +0100

    Arrays

---
+++ LearnJavaScript/Arrays/app.js
@@ -0,0 +1,8 @@
+//Define an array of studentd\s
+const students = ["Henry", "Ada", "Bright", "John", "Emeka"];
+//Write all the students to the webpage
+document.writeln(students);
+// Log students to the console
+console.log(students);
+// Log students at index 2 to the console
+console.log(students[2]);
