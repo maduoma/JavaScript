7c7e77269dc39842e91f6d847131cadaf288f0db
maduoma <18477889+maduoma@users.noreply.github.com>
2020-11-25 20:47:08 +0100

    JavaScript

---
+++ LearnJavaScript/app.js
@@ -0,0 +1,10 @@
+// document.write('Hello World!')
+// alert('Hello World!');
+// console.log('Hello world!');
+// prompt('Hello World!')
+
+document.writeln("Hello from write function!");
+window.alert("Hello from alert function!!");
+console.log("Hello World!");
+const name = prompt("What is your name?");
+document.write(name);
