63126111fbcccf6282537dc9512dc466235ed180
maduoma <18477889+maduoma@users.noreply.github.com>
2020-11-26 08:32:06 +0100

    Funstions

---
+++ LearnJavaScript/Functions/js/app.js
@@ -0,0 +1,14 @@
+"use strict";
+
+//Parameterized function declaration
+function calculate(value) {
+  const myValue = value * 2.54;
+  console.log("The value in cm is " + value * 2.54 + "cm");
+  return myValue;
+}
+//Method invocatuion
+const width = calculate(20);
+const height = calculate(30);
+//Prints undefined if there's no return type.
+const dimensions = [width, height];
+console.log(dimensions);
