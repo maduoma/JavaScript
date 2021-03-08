61fee6c38514f2a9b0de2404a463989da8d56e3d
maduoma <18477889+maduoma@users.noreply.github.com>
2021-03-08 08:18:46 +0100

    JS Basics

--- js-courses/js-basics/index.js
+++ js-courses/js-basics/index.js
@@ -89,7 +89,7 @@ var hour = new Date(
 //let dateTime = new Date();
 //let hour = 10;
 if (hour >= 6 && hour < 12) console.log("Good morning!");
-else if (hour >= 12 && hour < 18) console.log("Good afternoon!");
+else if (hour >= 12 && hour < 6) console.log("Good afternoon!");
 else console.log("Good evening!");
 
 printLabel("AM/PM Date format");
